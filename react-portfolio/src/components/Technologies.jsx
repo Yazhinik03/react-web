c9c106c31b2b4b9f5b48a7be88249508c54ec508
Yazhinik03 <yazhini2003k@gmail.com>
2024-09-25 11:48:49 +0530

    aa

--- react-portfolio/src/components/Technologies.jsx
+++ react-portfolio/src/components/Technologies.jsx
@@ -4,30 +4,74 @@ import { DiHtml5 } from "react-icons/di";
 import { FaPython } from "react-icons/fa";
 import { FaJsSquare } from "react-icons/fa";
 import { BiLogoMongodb } from "react-icons/bi";
+import { animate, inView, motion } from "framer-motion";
+const iconVariants = (duration)=>({
+  initial: {y:-10},
+  animate: {
+    y:[10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 className="my-20 text-center text-4xl">Technologies</h1>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.h1
+        whileInView={{opacity: 1, y:0}}
+        initial={{opacity: 0, y: -100}}
+        transition={{duration: 1.5}}
+      className="my-20 text-center text-4xl">Technologies</motion.h1>
+      <motion.div whileInView={{opacity: 1, x: 0}}
+      initial={{opacity: 0, x:-100}}
+      transition={{duration: 1.5}}
+       className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div 
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <RiReactjsLine className="text-4xl text-cyan-400"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(3)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <DiGithubBadge className="text-4xl"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(2)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <DiHtml5 className="text-4xl text-orange-400"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(6)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaPython className="text-4xl text-yellow-400"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(3.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaJsSquare className="text-4xl text-yellow-500"/>
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div 
+        variants={iconVariants(5.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-4">
             <BiLogoMongodb className="text-4xl text-green-500"/>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
--- react-portfolio/src/constants/index.js
+++ react-portfolio/src/constants/index.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/img/rfid.png";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+import project2 from "../assets/img/download.jpg";
+import project3 from "../assets/img/mal.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With hands-on experience, I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
